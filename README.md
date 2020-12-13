ae0be14ef22ae9c931deba55b6a6f507b8a3fe76
Anatoliy Platonov <p4m.dev@gmail.com>
2020-12-13 20:49:46 +0200

    Add readme description and fix code formating.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # gitignore-cli
-Tool to generate gitignore files for my projects
+Tool to generate gitignore files for my projects. Existing oh-my-zsh plugin makes HTTP request on each `Tab` press.
+It's no so cool and fast. This implementation saves all template ids to file in `.cache` directory and on `Tab` press reads ids from file.
+
+To install this plugin: 
+
+```bash
+make install
+```
+
+And then add `gitignore-rust` to `plugins` in `.zsh`
+
+Also you can run updating of ids with schedule. You must add `gitignore_teplates --save` to yours crontab.
+
+To remove this just run:
+
+```bash
+make uninstall
+```
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
-use clap::{Arg, App};
-use std::fs;
+use clap::{App, Arg};
 use reqwest;
-
+use std::fs;
 
 fn save_to_file() -> Result<(), reqwest::Error> {
-    let mut response = reqwest::blocking::get("https://www.gitignore.io/api/list")?
-        .text()?;
+    let mut response = reqwest::blocking::get("https://www.gitignore.io/api/list")?.text()?;
     response = response.replace(",", "\n");
-    fs::write("/home/anatoliy/.cache/gitignore_templates", response)
-        .expect("Save failed!!!");
+    fs::write("/home/anatoliy/.cache/gitignore_templates", response).expect("Save failed!!!");
     Ok(())
 }
 
 fn get_template_names() -> String {
     match fs::read_to_string("/home/anatoliy/.cache/gitignore_templates") {
         Ok(templates) => templates,
-        Err(_) => "".to_string()
+        Err(_) => "".to_string(),
     }
 }
 
@@ -24,13 +21,15 @@ fn main() {
         .version("0.1.0")
         .author("Platonov Anatoliy <p4m.dev@gmail.com>")
         .about("Can cache gitignore template names to ~/.cache folder")
-        .arg(Arg::with_name("save")
-            .short("s")
-            .long("save")
-            .takes_value(false)
-            .help("Loads template names and save it into cache file"))
+        .arg(
+            Arg::with_name("save")
+                .short("s")
+                .long("save")
+                .takes_value(false)
+                .help("Loads template names and save it into cache file"),
+        )
         .get_matches();
-    
+
     if cli_matches.is_present("save") {
         println!("--save found!!!");
         match save_to_file() {
